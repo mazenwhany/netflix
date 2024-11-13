d6f121f4be84bf0e92407c6f7a39277f5f6a4e98
mazenwhany <modr3a08@gmail.com>
2024-11-13 22:34:53 +0200

    1

--- lib/ui/widgets/now-playing.dart
+++ lib/ui/widgets/now-playing.dart
@@ -7,10 +7,9 @@ class nowplaying extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Image.network("https://s3-alpha-sig.figma.com/img/7bb1/9b45/51b7ed5e50b1b5f28c18a87b391af6d1?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CTqt5dEGiJ9Nej~5c8suBwmHcoHcWYdAU0LNxmeofYkMGx~ln2a32rwtDeZPjpaAiHfJ4juPxjNGRlEc~W8P-ATOktwYtv-NuK2ClVKEDcOtbkCUuNtawpjiL6znCJbvTxxaq9jLMEiMZoJIDHvLNC8AhKwnaEwoMPjlCp~2mr28iNXJTM0ycsvKfQAVRT3P3gZ~tfvd3lD99GRuorLAcdpb9OLNmxicCnr1i5Pt2y5Y4TX9yrE0tZsOJHACOG9W65D~pUs-BcTnpeT40g5RRGNquknfYf5fSvGSQpOQndHYT5S647j39QF9cdve9jeRQJAfweNBVqmd3NExb6qbmg__",
-          width: 420,
-          height: 370,
-          fit: BoxFit.fill,),
+            width: 420,
+            height: 370,
+            fit: BoxFit.fill,),
 
         Positioned(
           top: 328,
